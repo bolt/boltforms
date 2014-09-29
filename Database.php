7cc95d0698b2bd8ce0713fc0ab96f3b84d7340d6
Gawain Lynch <gawain.lynch@gmail.com>
2014-09-29 13:23:03 +0100

    Add outline for database interaction class
    Signed-off-by: Gawain Lynch <gawain.lynch@gmail.com>

---
+++ Database.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Bolt\Extension\Bolt\Forms;
+
+use Bolt;
+
+class Database
+{
+
+    /**
+     * @var Application
+     */
+    private $app;
+
+    /**
+     * @var array
+     */
+    private $config;
+
+    public function __construct(Silex\Application $app)
+    {
+        $this->app = $this->config = $app;
+        $this->config = $app[Extension::CONTAINER]->config;
+    }
+
+    public function writeToTable($tablename, $data)
+    {
+        // Don't try to write to a non-existant table
+        $sm = $this->app['db']->getSchemaManager();
+        if (! $sm->tablesExist(array($tablename))) {
+            return false;
+        }
+
+        // Don't try to insert NULLs
+        foreach($data as $key => $value) {
+            if ($value === null) {
+                $data[$key] = '';
+            }
+        }
+
+        //
+        $this->app['db']->insert($tablename, $data);
+    }
+
+    public function writeToContentype($contenttype, $data)
+    {
+        //
+        $record = $this->app['storage']->getEmptyContent($contenttype);
+        $record->setValues($values);
+        $id = $this->app['storage']->saveContent($record);
+    }
+}
