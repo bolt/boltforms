11b7fc2039981586031fb038ce6e2c835228d07f
Gawain Lynch <gawain.lynch@gmail.com>
2015-04-02 12:28:50 +0100

    Test extension register

---
+++ tests/ExtensionTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Bolt\Extension\Bolt\BoltForms\Tests;
+
+use Bolt\Tests\BoltUnitTest;
+use Bolt\Extension\Bolt\BoltForms\Extension;
+
+/**
+ * Ensure that BoltForms loads correctly.
+ *
+ */
+class ExtensionTest extends BoltUnitTest
+{
+    public function testExtensionRegister()
+    {
+        $app = $this->getApp();
+        $extension = new Extension($app);
+        $app['extensions']->register( $extension );
+        $this->assertSame($extension, $app['extensions.boltforms']);
+    }
+}
