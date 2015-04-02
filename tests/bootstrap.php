74b76c93ed5a31c9fbd1c2ae6e5e3d2ad56f4678
Gawain Lynch <gawain.lynch@gmail.com>
2015-04-02 12:28:37 +0100

    Specify vendor root

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,9 +2,10 @@
 
 define('TEST_ROOT', __DIR__ . '/tmp');
 define('PHPUNIT_ROOT', __DIR__);
+define('VENDOR_ROOT', realpath(dirname(__DIR__) . '/vendor'));
 
 @mkdir('tmp/app/cache', 0777, true);
 @mkdir('tmp/app/config', 0777, true);
 @mkdir('tmp/app/database', 0777, true);
 
-require '../vendor/autoload.php';
+require VENDOR_ROOT . '/autoload.php';
