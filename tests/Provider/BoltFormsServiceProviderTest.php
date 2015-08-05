7dcb09c7db6db2e824d373bb9b015ee1a83d13bd
Gawain Lynch <gawain.lynch@gmail.com>
2015-08-05 12:19:27 +0100

    BoltFormsServiceProvider tests

---
+++ tests/Provider/BoltFormsServiceProviderTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Bolt\Extension\Bolt\BoltForms\Tests;
+
+use Bolt\Tests\BoltUnitTest;
+use Bolt\Extension\Bolt\BoltForms\Extension;
+use Bolt\Extension\Bolt\BoltForms\BoltForms;
+use Bolt\Extension\Bolt\BoltForms\Database;
+use Bolt\Extension\Bolt\BoltForms\Email;
+use Bolt\Extension\Bolt\BoltForms\Subscriber\BoltFormsCustomDataSubscriber;
+
+/**
+ * BoltFormsServiceProvider tests.
+ *
+ * @author Gawain Lynch <gawain.lynch@gmail.com>
+ */
+class BoltFormsServiceProviderTest extends AbstractBoltFormsUnitTest
+{
+    public function testProviderRegister()
+    {
+        $app = $this->getApp();
+
+        $this->assertInstanceOf('\Bolt\Extension\Bolt\BoltForms\BoltForms', $app['boltforms']);
+        $this->assertInstanceOf('\Bolt\Extension\Bolt\BoltForms\Database', $app['boltforms.database']);
+        $this->assertInstanceOf('\Bolt\Extension\Bolt\BoltForms\Email', $app['boltforms.email']);
+        $this->assertInstanceOf('\Bolt\Extension\Bolt\BoltForms\Subscriber\BoltFormsCustomDataSubscriber', $app['boltforms.subscriber.custom_data']);
+    }
+}
