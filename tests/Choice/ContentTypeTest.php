3db167f1587a9c8f0a731daed2c6b6ce1834dcce
Gawain Lynch <gawain.lynch@gmail.com>
2015-08-05 13:20:44 +0100

    Test Choice\ContentType

---
+++ tests/Choice/ContentTypeTest.php
@@ -0,0 +1,59 @@
+<?php
+namespace Bolt\Extension\Bolt\BoltForms\Tests;
+
+use Bolt\Tests\BoltUnitTest;
+use Bolt\Extension\Bolt\BoltForms\Extension;
+use Bolt\Extension\Bolt\BoltForms\Choice\ContentType;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * ContentType choices test
+ *
+ * @author Gawain Lynch <gawain.lynch@gmail.com>
+ */
+class ContentTypeTest extends AbstractBoltFormsUnitTest
+{
+    public function testGetName()
+    {
+        $app = $this->getApp();
+        $obj = new ContentType($app, 'koala', 'contenttype::pages::title::slug');
+        $this->assertInstanceOf('\Bolt\Extension\Bolt\BoltForms\Choice\ContentType', $obj);
+        $this->assertSame($obj->getName(), 'koala');
+    }
+
+    /**
+     * @expectedException \UnexpectedValueException
+     */
+    public function testGetChoicesException()
+    {
+        $app = $this->getApp();
+        $obj = new ContentType($app, 'koala', 'contenttype');
+        $obj->getChoices();
+        $obj = new ContentType($app, 'koala', 'contenttype::pages');
+        $obj->getChoices();
+        $obj = new ContentType($app, 'koala', 'contenttype::pages::title');
+        $obj->getChoices();
+    }
+
+    public function testGetChoices()
+    {
+        $app = $this->getApp();
+        $app['request'] = Request::create('/');
+
+        $record = $app['storage']->getEmptyContent('pages');
+        $record->setValues(array('title' => 'Koala', 'slug' => 'gum-tree'));
+        $storage = $this->getMock('\Bolt\Storage', array('getContent'), array($app));
+        $storage
+            ->expects($this->any())
+            ->method('getContent')
+            ->will($this->returnValue(array($record)));
+        $app['storage'] = $storage;
+
+        $obj = new ContentType($app, 'koala', 'contenttype::pages::title::slug');
+        $choice = $obj->getChoices();
+
+        $this->assertNotEmpty($choice);
+        $this->assertArrayHasKey('gum-tree', $choice);
+        $this->assertSame('Koala', $choice['gum-tree']);
+    }
+}
