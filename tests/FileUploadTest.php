6cbed943d275524d3949554dfeccae49330a45c6
Gawain Lynch <gawain.lynch@gmail.com>
2015-08-06 15:43:12 +0100

    Test FileUpload

--- tests/FileUploadTest.php
+++ tests/FileUploadTest.php
@@ -3,199 +3,215 @@ namespace Bolt\Extension\Bolt\BoltForms\Tests;
 
 use Bolt\Tests\BoltUnitTest;
 use Bolt\Extension\Bolt\BoltForms\Extension;
-use Bolt\Extension\Bolt\BoltForms\BoltForms;
+use Bolt\Extension\Bolt\BoltForms\FileUpload;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\Filesystem\Filesystem;
 
 /**
- * BoltForms class tests.
+ * FileUpload class tests.
  *
  * @author Gawain Lynch <gawain.lynch@gmail.com>
  */
-class BoltFormsTest extends AbstractBoltFormsUnitTest
+class FileUploadTest extends AbstractBoltFormsUnitTest
 {
     public function testConstructor()
     {
         $app = $this->getApp();
-        $boltforms = new BoltForms($app);
+        $fileUpload = new UploadedFile(__FILE__, __FILE__, null, null, null, true);
+        $boltforms = new FileUpload($app, 'contact', $fileUpload);
 
-        $this->assertInstanceOf('\Bolt\Extension\Bolt\BoltForms\BoltForms', $boltforms);
+        $this->assertInstanceOf('\Bolt\Extension\Bolt\BoltForms\FileUpload', $boltforms);
     }
 
-    public function testMakeForm()
+    public function testBasicFunctions()
     {
         $app = $this->getApp();
-        $boltforms = new BoltForms($app);
+        $fileUpload = new UploadedFile(__FILE__, __FILE__, null, null, null, true);
+        $upload = new FileUpload($app, 'contact', $fileUpload);
 
-        $boltforms->makeForm('contact');
+        $this->assertSame(__FILE__, $upload->__toString());
+        $this->assertSame(__FILE__, $upload->fullPath());
+        $this->assertInstanceOf('\Symfony\Component\HttpFoundation\File\UploadedFile', $upload->getFile());
+        $this->assertTrue($upload->isValid());
     }
 
-    public function testGetForm()
+    public function testRelativePathExceptionDisabled()
     {
         $app = $this->getApp();
-        $boltforms = new BoltForms($app);
+        $this->getExtension($app)->config['uploads']['enabled'] = false;
 
-        $boltforms->makeForm('contact');
-        $form = $boltforms->getForm('contact');
+        $fileUpload = new UploadedFile(__FILE__, __FILE__, null, null, null, true);
+        $upload = new FileUpload($app, 'contact', $fileUpload);
 
-        $this->assertInstanceOf('\Symfony\Component\Form\Form', $form);
+        $this->setExpectedException( '\RuntimeException', 'The relative path is not valid when uploads are disabled!');
+        $upload->relativePath();
     }
 
-    private function formValues()
-    {
-        return array(
-            'name' => array(
-                'type'    => 'text',
-                'options' => array(
-                    'required' => true,
-                    'label'    => 'Name',
-                    'attr'     => array(
-                        'placeholder' => 'Your name...'
-                    ),
-                    'constraints' => array(
-                        'NotBlank',
-                        array(
-                            'Length' => array('min' => 3)
-                        )
-                    ),
-                ),
-            ),
-            'email' => array(
-                'type'    => 'email',
-                'options' => array(
-                    'required' => true,
-                    'label'    => 'Email address',
-                    'attr'     => array(
-                        'placeholder' => 'Your email...'
-                    ),
-                    'constraints' => 'Email',
-                ),
-            ),
-            'message' => array(
-                'type'    => 'textarea',
-                'options' => array(
-                    'required' => true,
-                    'label'    => 'Your message',
-                    'attr'     => array(
-                        'placeholder' => 'Your message...',
-                        'class'       => 'myclass'
-                    ),
-                ),
-            ),
-            'array_index' => array(
-                'type'    => 'choice',
-                'options' => array(
-                    'required' => false,
-                    'label'    => 'Should this test pass',
-                    'choices'  => array('Yes', 'No'),
-                    'multiple' => false
-                ),
-            ),
-            'array_assoc' => array(
-                'type'    => 'choice',
-                'options' => array(
-                    'required' => false,
-                    'label'    => 'What is cutest',
-                    'choices'  => array(
-                        'kittens' => 'Fluffy Kittens',
-                        'puppies' => 'Cute Puppies'
-                    ),
-                    'multiple' => false
-                ),
-            ),
-            'lookup' => array(
-                'type'    => 'choice',
-                'options' => array(
-                    'required' => false,
-                    'label'    => 'Select a record',
-                    'choices'  => 'contenttype::pages::title::slug',
-                    'multiple' => false
-                ),
-            ),
-            'submit' => array(
-                'type' => 'submit',
-                'options' => array()
-            ),
-        );
-    }
-
-    public function testAddFields()
+    public function testRelativePathExceptionEnabled()
     {
         $app = $this->getApp();
-        $app['request'] = Request::create('/');
-        $boltforms = new BoltForms($app);
+        $this->getExtension($app)->config['uploads']['enabled'] = true;
 
-        $boltforms->makeForm('contact');
-        $fields = $this->formValues();
+        $fileUpload = new UploadedFile(__FILE__, __FILE__, null, null, null, true);
+        $upload = new FileUpload($app, 'contact', $fileUpload);
 
-        $boltforms->addFieldArray('contact', $fields);
+        $this->setExpectedException( '\RuntimeException', 'The relative path is not valid before the file is moved!');
+        $upload->relativePath();
+    }
 
-        $form = $boltforms->getForm('contact');
+    public function testRelativePath()
+    {
+        $app = $this->getApp();
+        $this->getExtension($app)->config['uploads']['enabled'] = true;
+        $this->getExtension($app)->config['uploads']['base_directory'] = dirname(__FILE__);
 
-        foreach ($fields as $field => $values) {
-            $this->assertTrue($form->has($field));
+        $fileUpload = new UploadedFile(__FILE__, __FILE__, null, null, null, true);
+        $upload = new FileUpload($app, 'contact', $fileUpload);
+
+        $path = $upload->relativePath();
+        $this->assertSame($path, basename(__FILE__));
+    }
+
+    public function testMoveUploadedFileBaseDirectory()
+    {
+        $app = $this->getApp();
+        $this->getExtension($app)->config['uploads']['base_directory'] = sys_get_temp_dir();
+        $srcFile = EXTENSION_TEST_ROOT . '/tests/data/bolt-logo.png';
+        $tmpFile = sys_get_temp_dir() . '/' . uniqid('php_');
+
+        $fs = new Filesystem();
+        $fs->copy($srcFile, $tmpFile, true);
+
+        $fileUpload = new UploadedFile($tmpFile, 'bolt-logo.png', null, null, null, true);
+        $upload = new FileUpload($app, 'contact', $fileUpload);
+        $upload->move();
+
+        $this->assertFileExists($upload->fullPath());
+    }
+
+    public function testMoveUploadedFileSubDirectory()
+    {
+        $app = $this->getApp();
+        $this->getExtension($app)->config['uploads']['base_directory'] = sys_get_temp_dir();
+        $this->getExtension($app)->config['contact']['uploads']['subdirectory'] = 'contact';
+
+        $srcFile = EXTENSION_TEST_ROOT . '/tests/data/bolt-logo.png';
+        $tmpFile = sys_get_temp_dir() . '/' . uniqid('php_');
+
+        $fs = new Filesystem();
+        $fs->copy($srcFile, $tmpFile, true);
+
+        $fileUpload = new UploadedFile($tmpFile, 'bolt-logo.png', null, null, null, true);
+        $upload = new FileUpload($app, 'contact', $fileUpload);
+        $upload->move();
+
+        $this->assertFileExists($upload->fullPath());
+    }
+
+    public function testMoveUploadedFilePrefix()
+    {
+        $app = $this->getApp();
+        $this->getExtension($app)->config['uploads']['base_directory'] = sys_get_temp_dir();
+        $this->getExtension($app)->config['contact']['uploads']['subdirectory'] = 'contact';
+        $this->getExtension($app)->config['uploads']['filename_handling'] = 'prefix';
+
+        $srcFile = EXTENSION_TEST_ROOT . '/tests/data/bolt-logo.png';
+        $tmpFile = sys_get_temp_dir() . '/' . uniqid('php_');
+        $tmpDir = sys_get_temp_dir() . '/contact';
+
+        $fs = new Filesystem();
+        if ($fs->exists($tmpDir)) {
+            $fs->remove($tmpDir);
         }
+        $fs->copy($srcFile, $tmpFile, true);
+
+        $fileUpload = new UploadedFile($tmpFile, 'bolt-logo.png', null, null, null, true);
+        $upload = new FileUpload($app, 'contact', $fileUpload);
+        $upload->move();
+
+        $this->assertFileExists($upload->fullPath());
+        $this->assertRegExp('#\b(?:bolt-logo\.)[a-zA-Z0-9]{12}(?:\.png)\b#', basename($upload->fullPath()));
     }
 
-    public function testRenderForm()
+    public function testMoveUploadedFileSufix()
     {
         $app = $this->getApp();
-        $app['request'] = Request::create('/');
-        $boltforms = new BoltForms($app);
+        $this->getExtension($app)->config['uploads']['base_directory'] = sys_get_temp_dir();
+        $this->getExtension($app)->config['contact']['uploads']['subdirectory'] = 'contact';
+        $this->getExtension($app)->config['uploads']['filename_handling'] = 'suffix';
 
-        $boltforms->makeForm('contact');
-        $fields = $this->formValues();
-        $boltforms->addFieldArray('contact', $fields);
+        $srcFile = EXTENSION_TEST_ROOT . '/tests/data/bolt-logo.png';
+        $tmpFile = sys_get_temp_dir() . '/' . uniqid('php_');
+        $tmpDir = sys_get_temp_dir() . '/contact';
 
-        $html = $boltforms->renderForm('contact', null, array('recaptcha' => array('enabled' => true)));
-        $this->assertInstanceOf('\Twig_Markup', $html);
-        $html = (string) $html;
+        $fs = new Filesystem();
+        if ($fs->exists($tmpDir)) {
+            $fs->remove($tmpDir);
+        }
+        $fs->copy($srcFile, $tmpFile, true);
 
-        $this->assertRegExp('#<link href="/extensions/vendor/bolt/boltforms/css/boltforms.css" rel="stylesheet" type="text/css" />#', $html);
-        $this->assertRegExp('#<div class="boltform">#', $html);
-        $this->assertRegExp('#var RecaptchaOptions =#', $html);
-        $this->assertRegExp('#<form method="post" action="" name="">#', $html);
-        $this->assertRegExp('#<ul class="boltform-error">#', $html);
-        $this->assertRegExp('#<li class="boltform-errors"></li>#', $html);
-        $this->assertRegExp('#<label for="form_message" class="required"></label>#', $html);
-        $this->assertRegExp('#<script src="https://www.google.com/recaptcha/api.js\?hl=en-GB" async defer></script>#', $html);
-        $this->assertRegExp('#<div class="g-recaptcha" data-sitekey=""></div>#', $html);
-        $this->assertRegExp('#<div><button type="submit" id="contact_submit" name="contact\[submit\]">Submit</button></div>#', $html);
-        $this->assertRegExp('#<label for="contact_name" class="required">Name</label>#', $html);
-        $this->assertRegExp('#<input type="text" id="contact_name" name="contact\[name\]" required="required"    placeholder="Your name..." />#', $html);
-        $this->assertRegExp('#<label for="contact_email" class="required">Email address</label>#', $html);
-        $this->assertRegExp('#<input type="email" id="contact_email" name="contact\[email\]" required="required"    placeholder="Your email..." />#', $html);
-        $this->assertRegExp('#<label for="contact_message" class="required">Your message</label>#', $html);
-        $this->assertRegExp('#<textarea id="contact_message" name="contact\[message\]" required="required"    placeholder="Your message..." class="myclass"></textarea>#', $html);
-        $this->assertRegExp('#<label for="contact_array_index">Should this test pass</label>#', $html);
-        $this->assertRegExp('#<select id="contact_array_index" name="contact\[array_index\]"><option  value=""></option><option value="0">Yes</option><option value="1">No</option></select>#', $html);
-        $this->assertRegExp('#<label for="contact_array_assoc">What is cutest</label>#', $html);
-        $this->assertRegExp('#<select id="contact_array_assoc" name="contact\[array_assoc\]"><option  value=""></option><option value="kittens">Fluffy Kittens</option><option value="puppies">Cute Puppies</option></select>#', $html);
-        $this->assertRegExp('#<label for="contact_lookup">Select a record</label>#', $html);
-        $this->assertRegExp('#<select id="contact_lookup" name="contact\[lookup\]"><option  value=""></option></select>#', $html);
-        $this->assertRegExp('#<input type="hidden" id="contact__token" name="contact\[_token\]" value=#', $html);
+        $fileUpload = new UploadedFile($tmpFile, 'bolt-logo.png', null, null, null, true);
+        $upload = new FileUpload($app, 'contact', $fileUpload);
+        $upload->move();
+
+        $this->assertFileExists($upload->fullPath());
+        $this->assertRegExp('#\b(?:bolt-logo\.png\.)[a-zA-Z0-9]{12}\b#', basename($upload->fullPath()));
     }
 
-    public function testProcessRequest()
+    public function testMoveUploadedFileKeep()
     {
         $app = $this->getApp();
-        $this->getExtension($app)->config['csrf'] = false;
-        $app['request'] = Request::create('/');
-        $boltforms = new BoltForms($app);
-        $boltforms->makeForm('contact');
-        $fields = $this->formValues();
-        $boltforms->addFieldArray('contact', $fields);
+        $this->getExtension($app)->config['uploads']['base_directory'] = sys_get_temp_dir();
+        $this->getExtension($app)->config['uploads']['filename_handling'] = 'keep';
+        $this->getExtension($app)->config['contact']['uploads']['subdirectory'] = 'contact';
 
-        $parameters = array(
-            'contact' => array(
-                'name'    => 'Gawain Lynch',
-                'email'   => 'gawain.lynch@gmail.com',
-                'message' => 'Hello'
-            )
-        );
-        $app['request'] = Request::create('/', 'POST', $parameters);
+        $srcFile = EXTENSION_TEST_ROOT . '/tests/data/bolt-logo.png';
+        $tmpFile = sys_get_temp_dir() . '/' . uniqid('php_');
+        $tmpDir = sys_get_temp_dir() . '/contact';
 
-        $result = $boltforms->processRequest('contact', array('success' => true));
+        $fs = new Filesystem();
+        if ($fs->exists($tmpDir)) {
+            $fs->remove($tmpDir);
+        }
+        $fs->copy($srcFile, $tmpFile, true);
 
-        $this->assertTrue($result);
+        $fileUpload = new UploadedFile($tmpFile, 'bolt-logo.png', null, null, null, true);
+        $upload = new FileUpload($app, 'contact', $fileUpload);
+        $upload->move();
+
+        $this->assertFileExists($upload->fullPath());
+        $this->assertSame(basename($upload->fullPath()), basename($srcFile));
+    }
+
+    public function testMoveUploadedFileDuplicates()
+    {
+        $app = $this->getApp();
+        $this->getExtension($app)->config['uploads']['base_directory'] = sys_get_temp_dir();
+        $this->getExtension($app)->config['uploads']['filename_handling'] = 'keep';
+        $this->getExtension($app)->config['contact']['uploads']['subdirectory'] = 'contact';
+
+        $srcFile = EXTENSION_TEST_ROOT . '/tests/data/bolt-logo.png';
+        $tmpFile = sys_get_temp_dir() . '/' . uniqid('php_');
+        $tmpDir = sys_get_temp_dir() . '/contact';
+
+        $fs = new Filesystem();
+        if ($fs->exists($tmpDir)) {
+            $fs->remove($tmpDir);
+        }
+        $fs->copy($srcFile, $tmpFile, true);
+
+        $fileUpload = new UploadedFile($tmpFile, 'bolt-logo.png', null, null, null, true);
+        $upload = new FileUpload($app, 'contact', $fileUpload);
+        $upload->move();
+
+        $fs->copy($srcFile, $tmpFile, true);
+
+        $fileUpload = new UploadedFile($tmpFile, 'bolt-logo.png', null, null, null, true);
+        $upload = new FileUpload($app, 'contact', $fileUpload);
+        $upload->move();
+
+        $this->assertFileExists($upload->fullPath());
+        $this->assertSame(basename($upload->fullPath()), 'bolt-logo(1).png');
     }
 }
