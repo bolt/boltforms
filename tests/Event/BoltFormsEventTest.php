5d2fad564d19774b6c369efe12a96d86dec90d53
Gawain Lynch <gawain.lynch@gmail.com>
2015-08-08 05:05:43 +0100

    Test BoltFormsEvent

---
+++ tests/Event/BoltFormsEventTest.php
@@ -0,0 +1,41 @@
+<?php
+namespace Bolt\Extension\Bolt\BoltForms\Tests;
+
+use Bolt\Extension\Bolt\BoltForms\BoltForms;
+use Bolt\Extension\Bolt\BoltForms\Event\BoltFormsEvent;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * BoltFormsEvent class tests.
+ *
+ * @author Gawain Lynch <gawain.lynch@gmail.com>
+ */
+class BoltFormsEventTest extends AbstractBoltFormsUnitTest
+{
+    public function testConstructor()
+    {
+        $app = $this->getApp();
+        $app['request'] = Request::create('/');
+        $boltforms = new BoltForms($app);
+
+        $boltforms->makeForm('contact');
+        $fields = $this->formValues();
+        $boltforms->addFieldArray('contact', $fields);
+
+        $evt = new FormEvent($boltforms->getForm('contact'), array('koala' => 'leaves'));
+        $event = new BoltFormsEvent($evt);
+
+        $this->assertInstanceOf('Symfony\Component\Form\FormEvent', $event->getEvent());
+        $this->assertInstanceOf('Symfony\Component\Form\FormInterface', $event->getForm());
+        $this->assertSame(array('koala' => 'leaves'), $event->getData());
+
+        $event->getEvent()->setName(FormEvents::PRE_SUBMIT);
+        $event->setData('fresh');
+
+        $this->setExpectedException('\RuntimeException');
+        $event->getEvent()->setName(FormEvents::POST_SUBMIT);
+        $event->setData('fail');
+    }
+}
