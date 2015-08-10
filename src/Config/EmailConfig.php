2634fc0a7fcf0042244fbd22fc5361aeb678d0e9
Gawain Lynch <gawain.lynch@gmail.com>
2015-08-10 10:21:00 +0100

    Move email configuration data to an object class

---
+++ src/Config/EmailConfig.php
@@ -0,0 +1,270 @@
+<?php
+namespace Bolt\Extension\Bolt\BoltForms\Config;
+
+use Bolt\Extension\Bolt\BoltForms\Exception\EmailException;
+
+/**
+ * Email configuration for BoltForms
+ *
+ * Copyright (C) 2014-2015 Gawain Lynch
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author    Gawain Lynch <gawain.lynch@gmail.com>
+ * @copyright Copyright (c) 2014, Gawain Lynch
+ * @license   http://opensource.org/licenses/GPL-3.0 GNU Public License 3.0
+ */
+class EmailConfig
+{
+    /** @var array */
+    protected $globalConfig;
+    /** @var array */
+    protected $formConfig;
+    /** @var array */
+    protected $formData;
+    /** @var boolean */
+    protected $debug;
+    /** @var string */
+    protected $debugAddress;
+    /** @var string */
+    protected $toName;
+    /** @var string */
+    protected $toEmail;
+    /** @var string */
+    protected $fromName;
+    /** @var string */
+    protected $fromEmail;
+    /** @var string */
+    protected $ccName;
+    /** @var string */
+    protected $ccEmail;
+    /** @var string */
+    protected $bccName;
+    /** @var string */
+    protected $bccEmail;
+    /** @var string */
+    protected $replyToName;
+    /** @var string */
+    protected $replyToEmail;
+
+    public function __construct(array $globalConfig, array $formConfig, array $formData)
+    {
+        $this->globalConfig = $globalConfig;
+        $this->formConfig = $formConfig;
+        $this->formData = $formData;
+
+        $this->setDebugState();
+        $this->setEmailConfig();
+    }
+
+    /**
+     * Set the debugging state.
+     *
+     * Global debug enabled
+     *   - Messages should always go to the global debug address only
+     *   - Takes preference over form specific settings
+     *   - To address also takes precidence
+     *
+     * Global debug disabled
+     *   - Form specific debug settings are applied
+     *
+     * Form debug enabled
+     *   - For debug address takes priority if set
+     *
+     * @throws EmailException
+     */
+    protected function setDebugState()
+    {
+        if ($this->globalConfig['debug']['enabled']) {
+            $this->debug = true;
+
+            if (empty($this->globalConfig['debug']['address'])) {
+                throw new EmailException('[BoltForms] Debug email address can not be empty if debugging enabled!');
+            } else {
+                $this->debugAddress = $this->globalConfig['debug']['address'];
+            }
+        } else {
+            $this->debug = isset($this->formConfig['debug']) && $this->formConfig['debug'];
+
+            if (isset($this->formConfig['debug_address'])) {
+                $this->debugAddress = $this->formConfig['debug_address'];
+            } else {
+                $this->debugAddress = $this->globalConfig['debug']['address'];
+            }
+        }
+    }
+
+    /**
+     * Debugging state.
+     *
+     * @return boolean
+     */
+    public function isDebug()
+    {
+        return $this->debug;
+    }
+
+    /**
+     * Get the debugging email address.
+     *
+     * @return string
+     */
+    public function getDebugAddress()
+    {
+        return $this->debugAddress;
+    }
+
+    /**
+     * Get the 'from' name.
+     *
+     * @return string
+     */
+    public function getFromName()
+    {
+        return $this->fromName;
+    }
+
+    /**
+     * Get the 'from' email address.
+     *
+     * @return string
+     */
+    public function getFromEmail()
+    {
+        return $this->fromEmail;
+    }
+
+    /**
+     * Get the 'replyto' name.
+     *
+     * @return string
+     */
+    public function getReplyToName()
+    {
+        return $this->replyToName;
+    }
+
+    /**
+     * Get the 'replyto' email address.
+     *
+     * @return string
+     */
+    public function getReplyToEmail()
+    {
+        return $this->replyToEmail;
+    }
+
+    /**
+     * Get the 'To' name.
+     *
+     * @return string
+     */
+    public function getToName()
+    {
+        return $this->toName;
+    }
+
+    /**
+     * Get the 'To' email address.
+     *
+     * @return string
+     */
+    public function getToEmail()
+    {
+        return $this->toEmail;
+    }
+
+    /**
+     * Get the 'CC' name.
+     *
+     * @return string
+     */
+    public function getCcName()
+    {
+        return $this->ccName;
+    }
+
+    /**
+     * Get the 'CC' email address.
+     *
+     * @return string
+     */
+    public function getCcEmail()
+    {
+        return $this->ccEmail;
+    }
+
+    /**
+     * Get the 'bCC' name.
+     *
+     * @return string
+     */
+    public function getBccName()
+    {
+        return $this->bccName;
+    }
+
+    /**
+     * Get the 'bCC' email address.
+     *
+     * @return string
+     */
+    public function getBccEmail()
+    {
+        return $this->bccEmail;
+    }
+
+    /**
+     * Get resolved email configuration settings.
+     */
+    private function setEmailConfig()
+    {
+        $notify = $this->formConfig['notification'];
+
+        $hashMap = array(
+            'fromName'     => 'from_name',
+            'fromEmail'    => 'from_email',
+            'replyToName'  => 'replyto_name',
+            'replyToEmail' => 'replyto_email',
+            'toName'       => 'to_name',
+            'toEmail'      => 'to_email',
+            'ccName'       => 'cc_name',
+            'ccEmail'      => 'cc_email',
+            'bccName'      => 'bcc_name',
+            'bccEmail'     => 'bcc_email',
+        );
+
+        foreach ($hashMap as $property => $key) {
+            $this->{$property} = $this->getConfigValue($notify[$key]);
+        }
+
+    }
+
+    /**
+     * Get a resolved field value.
+     *
+     * If the form notification configuration wants a value to be returned from
+     * a submitted field we use this, otherwise the configured parameter.
+     *
+     * @param string $value
+     */
+    private function getConfigValue($value)
+    {
+        if (isset($this->formData[$value])) {
+            return $this->formData[$value];
+        }
+
+        return $value;
+    }
+}
