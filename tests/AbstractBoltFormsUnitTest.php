8b21943d4d53e8e77f91d82c99f8498cec2a41fe
Gawain Lynch <gawain.lynch@gmail.com>
2015-08-10 13:21:00 +0100

    Add replyto to tests

--- tests/AbstractBoltFormsUnitTest.php
+++ tests/AbstractBoltFormsUnitTest.php
@@ -32,6 +32,25 @@ abstract class AbstractBoltFormsUnitTest extends BoltUnitTest
         return $app["extensions.BoltForms"];
     }
 
+    protected function formNotificationConfig()
+    {
+        return array(
+            'enabled' => true,
+            'debug' => false,
+            'from_name' => 'Gawain Lynch',
+            'from_email' => 'gawain@example.com',
+            'replyto_name' => 'Surprised Koala',
+            'replyto_email' => 'surprised.koala@example.com',
+            'to_name' => 'Kenny Koala',
+            'to_email' => 'kenny.koala@example.com',
+            'cc_name' => 'Bob den Otter',
+            'cc_email' => 'bob@example.com',
+            'bcc_name' => 'Lodewijk Evers',
+            'bcc_email' => 'lodewijk@example.com',
+            'attach_files' => true,
+        );
+    }
+
     protected function formFieldConfig()
     {
         return array(
