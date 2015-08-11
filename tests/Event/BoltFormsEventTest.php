d3d13dc110df587d5116119d20d5c5ccda31d896
Gawain Lynch <gawain.lynch@gmail.com>
2015-08-11 11:15:56 +0100

    Update event tests

--- tests/Event/BoltFormsEventTest.php
+++ tests/Event/BoltFormsEventTest.php
@@ -14,7 +14,7 @@ use Symfony\Component\HttpFoundation\Request;
  */
 class BoltFormsEventTest extends AbstractBoltFormsUnitTest
 {
-    public function testConstructor()
+    public function testEventValidSetData()
     {
         $app = $this->getApp();
         $app['request'] = Request::create('/');
@@ -25,14 +25,31 @@ class BoltFormsEventTest extends AbstractBoltFormsUnitTest
         $boltforms->addFieldArray('testing_form', $fields);
 
         $evt = new FormEvent($boltforms->getForm('testing_form'), array('koala' => 'leaves'));
-        $event = new BoltFormsEvent($evt);
+        $event = new BoltFormsEvent($evt, FormEvents::PRE_SUBMIT);
 
         $this->assertInstanceOf('Symfony\Component\Form\FormEvent', $event->getEvent());
         $this->assertInstanceOf('Symfony\Component\Form\FormInterface', $event->getForm());
         $this->assertSame(array('koala' => 'leaves'), $event->getData());
 
-        $event->getEvent()->setName(FormEvents::PRE_SUBMIT);
         $event->setData('fresh');
+    }
+
+    public function testEventInvalidSetData()
+    {
+        $app = $this->getApp();
+        $app['request'] = Request::create('/');
+        $boltforms = new BoltForms($app);
+
+        $boltforms->makeForm('testing_form');
+        $fields = $this->formFieldConfig();
+        $boltforms->addFieldArray('testing_form', $fields);
+
+        $evt = new FormEvent($boltforms->getForm('testing_form'), array('koala' => 'leaves'));
+        $event = new BoltFormsEvent($evt, FormEvents::POST_SUBMIT);
+
+        $this->assertInstanceOf('Symfony\Component\Form\FormEvent', $event->getEvent());
+        $this->assertInstanceOf('Symfony\Component\Form\FormInterface', $event->getForm());
+        $this->assertSame(array('koala' => 'leaves'), $event->getData());
 
         $this->setExpectedException('\RuntimeException');
         $event->getEvent()->setName(FormEvents::POST_SUBMIT);
