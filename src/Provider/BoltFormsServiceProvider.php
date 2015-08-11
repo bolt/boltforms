c1bb7149fed54306b0baf92850ac5d98c49324cb
Gawain Lynch <gawain.lynch@gmail.com>
2015-08-11 14:29:41 +0100

    Add processor to service provider

--- src/Provider/BoltFormsServiceProvider.php
+++ src/Provider/BoltFormsServiceProvider.php
@@ -5,6 +5,7 @@ namespace Bolt\Extension\Bolt\BoltForms\Provider;
 use Bolt\Extension\Bolt\BoltForms\BoltForms;
 use Bolt\Extension\Bolt\BoltForms\Database;
 use Bolt\Extension\Bolt\BoltForms\Email;
+use Bolt\Extension\Bolt\BoltForms\Submission\Processor;
 use Bolt\Extension\Bolt\BoltForms\Subscriber\BoltFormsCustomDataSubscriber;
 use Silex\Application;
 use Silex\ServiceProviderInterface;
@@ -18,6 +19,14 @@ class BoltFormsServiceProvider implements ServiceProviderInterface
                 $forms = new BoltForms($app);
 
                 return $forms;
+            }
+        );
+
+        $app['boltforms.processor'] = $app->share(
+            function ($app) {
+                $processor = new Processor($app);
+
+                return $processor;
             }
         );
 
