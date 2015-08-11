3e1da31501a6ee89a9b1c3a3908c409f5080c3f2
Gawain Lynch <gawain.lynch@gmail.com>
2015-08-11 10:27:37 +0100

    Expand database test for custom data subscriber

--- tests/Subscriber/BoltFormsCustomDataSubscriberTest.php
+++ tests/Subscriber/BoltFormsCustomDataSubscriberTest.php
@@ -3,12 +3,130 @@
 namespace Bolt\Extension\Bolt\BoltForms\Tests;
 
 use Bolt\Extension\Bolt\BoltForms\BoltForms;
+use Bolt\Extension\Bolt\BoltForms\Subscriber\BoltFormsCustomDataSubscriber;
 use Bolt\Tests\Mocks\DoctrineMockBuilder;
 use Symfony\Component\HttpFoundation\Request;
 
 class BoltFormsCustomDataSubscriberTest extends AbstractBoltFormsUnitTest
 {
+    public function testNextIncrementInvalidParameters()
+    {
+        $app = $this->getApp();
+
+        $event = $this->getMock('\Bolt\Extension\Bolt\BoltForms\Event\BoltFormsCustomDataEvent',
+            array('setData'),
+            array('testevent', array()));
+        $event->expects($this->never())
+            ->method('setData');
+
+        $sub = new BoltFormsCustomDataSubscriber($app);
+
+        $sub->nextIncrement($event);
+    }
+
+    public function testNextIncrementInvalidEmptyTableName()
+    {
+        $app = $this->getApp();
+
+        $event = $this->getMock('\Bolt\Extension\Bolt\BoltForms\Event\BoltFormsCustomDataEvent',
+            array('setData'),
+            array('testevent', array('table' => '')));
+        $event->expects($this->never())
+            ->method('setData');
+
+        $logger = $this->getMock('\Monolog\Logger', array('error'), array('testlogger'));
+        $logger->expects($this->atLeastOnce())
+            ->method('error');
+        $app['logger.system'] = $logger;
+
+        $sub = new BoltFormsCustomDataSubscriber($app);
+
+        $sub->nextIncrement($event);
+    }
+
+    public function testNextIncrementInvalidQuery()
+    {
+        $app = $this->getApp();
+
+        $event = $this->getMock('\Bolt\Extension\Bolt\BoltForms\Event\BoltFormsCustomDataEvent',
+            array('setData'),
+            array('testevent', array('table' => 'koala')));
+        $event->expects($this->never())
+            ->method('setData');
+
+        $logger = $this->getMock('\Monolog\Logger', array('error'), array('testlogger'));
+        $logger->expects($this->atLeastOnce())
+            ->method('error');
+        $app['logger.system'] = $logger;
+
+        $sub = new BoltFormsCustomDataSubscriber($app);
+
+        $sub->nextIncrement($event);
+    }
+
     public function testNextIncrementTable()
+    {
+        $nextIncField = array(
+            'type'    => 'hidden',
+            'options' => array('label' => false),
+            'event'   => array(
+                'name'   => 'next_increment',
+                'params' => array(
+                    'table'  => 'koalas',
+                    'column' => 'gum_leaves'
+                )
+            ),
+        );
+
+        $app = $this->getApp(false);
+        $this->getExtension($app)->config['csrf'] = false;
+        $this->getExtension($app)->config['testing_form']['fields']['next_inc'] = $nextIncField;
+
+        $app['request'] = Request::create('/');
+        $boltforms = new BoltForms($app);
+        $boltforms->makeForm('testing_form');
+
+        $fields = $this->formFieldConfig();
+        $fields['next_inc'] = $nextIncField;
+
+        $boltforms->addFieldArray('testing_form', $fields);
+
+        $parameters = $this->formData();
+        $app['request'] = Request::create('/', 'POST', $parameters);
+        $app->boot();
+
+        // Mock the database query
+        $mocker = new DoctrineMockBuilder();
+        $db = $mocker->getConnectionMock();
+        $queries = array();
+
+        $db->expects($this->any())
+            ->method('executeQuery')
+            ->will($this->returnCallback(
+                function ($query, $params) use (&$queries, $mocker) {
+                    $queries[] = $query;
+
+                    return $mocker->getStatementMock(22);
+                }
+        ));
+
+        $app['db'] = $db;
+
+        // Mock Bolt\Users
+        $users = $this->getMock('\Bolt\Users', array('getUsers'), array($app));
+        $users->expects($this->any())
+            ->method('getUsers')
+            ->willReturn(array('id' => 1));
+        $app['users'] = $users;
+
+        $result = $boltforms->processRequest('testing_form', array('success' => true), true);
+
+        $this->assertEquals('SELECT MAX(gum_leaves) as max FROM koalas', $queries[0]);
+        $this->assertArrayHasKey('next_inc', $result);
+        $this->assertSame(23, $result->get('next_inc'));
+    }
+
+    public function testNextIncrementTableMin()
     {
         $nextIncField = array(
             'type'    => 'hidden',
@@ -44,19 +162,17 @@ class BoltFormsCustomDataSubscriberTest extends AbstractBoltFormsUnitTest
         $mocker = new DoctrineMockBuilder();
         $db = $mocker->getConnectionMock();
         $queries = array();
+
         $db->expects($this->any())
             ->method('executeQuery')
             ->will($this->returnCallback(
                 function ($query, $params) use (&$queries, $mocker) {
                     $queries[] = $query;
 
-                    return $mocker->getStatementMock();
+                    return $mocker->getStatementMock(42);
                 }
         ));
-        $db->expects($this->any())
-            ->method('fetchColumn')
-            ->with($this->equalTo('koalas'))
-            ->willReturn(55);
+
         $app['db'] = $db;
 
         // Mock Bolt\Users
@@ -70,7 +186,7 @@ class BoltFormsCustomDataSubscriberTest extends AbstractBoltFormsUnitTest
 
         $this->assertEquals('SELECT MAX(gum_leaves) as max FROM koalas', $queries[0]);
         $this->assertArrayHasKey('next_inc', $result);
-        $this->assertSame(42, $result->get('next_inc'));
+        $this->assertSame(43, $result->get('next_inc'));
     }
 
     public function testNextIncrementContentType()
