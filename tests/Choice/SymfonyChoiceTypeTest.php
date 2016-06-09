eccddf5b6a5bf6ef60f139b30252980f723cfcf4
Gawain Lynch <gawain.lynch@gmail.com>
2016-06-09 14:58:36 +0200

    [Tests] Rename ArrayType to SymfonyChoiceType

--- tests/Choice/SymfonyChoiceTypeTest.php
+++ tests/Choice/SymfonyChoiceTypeTest.php
@@ -1,25 +1,25 @@
 <?php
 namespace Bolt\Extension\Bolt\BoltForms\Tests;
 
-use Bolt\Extension\Bolt\BoltForms\Choice\ArrayType;
+use Bolt\Extension\Bolt\BoltForms\Choice\SymfonyChoiceType;
 
 /**
  * Array choices test
  *
  * @author Gawain Lynch <gawain.lynch@gmail.com>
  */
-class ArrayTypeTest extends AbstractBoltFormsUnitTest
+class SymfonyChoiceTypeTest extends AbstractBoltFormsUnitTest
 {
     public function testGetName()
     {
-        $obj = new ArrayType('koala', array('gum', 'leaves'));
+        $obj = new SymfonyChoiceType('koala', array('gum', 'leaves'));
         $this->assertInstanceOf('\Bolt\Extension\Bolt\BoltForms\Choice\ArrayType', $obj);
         $this->assertSame($obj->getName(), 'koala');
     }
 
     public function testGetChoices()
     {
-        $obj = new ArrayType('koala', array('gum', 'leaves'));
+        $obj = new SymfonyChoiceType('koala', array('gum', 'leaves'));
         $choices = $obj->getChoices();
 
         $this->assertContains('gum', $choices);
