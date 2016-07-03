f49dc6618c95fce5c719147d40aac61e6b908fd5
Gawain Lynch <gawain.lynch@gmail.com>
2016-07-03 09:46:28 +0200

    Constructor updates

--- src/Provider/BoltFormsServiceProvider.php
+++ src/Provider/BoltFormsServiceProvider.php
@@ -124,19 +124,63 @@ class BoltFormsServiceProvider implements ServiceProviderInterface
         /** @deprecated Since 3.1 and to be removed in 4.0. Use $app['boltforms.handlers']['email'] instead. */
         $app['boltforms.email'] = $app->share(
             function ($app) {
-                $email = new Submission\Handler\Email($app['boltforms.config'], $app['storage'], $app['boltforms.feedback'], $app['logger.system'], $app['mailer']);
-
-                return $email;
+                return new Submission\Handler\Email(
+                    $app['boltforms.config'],
+                    $app['storage'],
+                    $app['boltforms.feedback'],
+                    $app['logger.system'],
+                    $app['mailer'],
+                    $app['dispatcher'],
+                    $app['twig'],
+                    $app['url_generator']
+                );
             }
         );
 
         $app['boltforms.handlers'] = $app->share(
             function ($app) {
                 return new Container([
-                    'content'  => $app->share(function () use ($app) { return new Submission\Handler\ContentType($app['boltforms.config'], $app['storage'], $app['boltforms.feedback'], $app['logger.system'], $app['mailer']); }),
-                    'database' => $app->share(function () use ($app) { return new Submission\Handler\DatabaseTable($app['boltforms.config'], $app['storage'], $app['boltforms.feedback'], $app['logger.system'], $app['mailer']); }),
-                    'email'    => $app->share(function () use ($app) { return new Submission\Handler\Email($app['boltforms.config'], $app['storage'], $app['boltforms.feedback'], $app['logger.system'], $app['mailer']); }),
-                    'redirect' => $app->share(function () use ($app) { return new Submission\Handler\Redirect($app['url_matcher']); }),
+                    'content'  => $app->share(
+                        function () use ($app) {
+                            return new Submission\Handler\ContentType(
+                                $app['boltforms.config'],
+                                $app['storage'],
+                                $app['boltforms.feedback'],
+                                $app['logger.system'],
+                                $app['mailer']
+                            );
+                        }
+                    ),
+                    'database' => $app->share(
+                        function () use ($app) {
+                            return new Submission\Handler\DatabaseTable(
+                                $app['boltforms.config'],
+                                $app['storage'],
+                                $app['boltforms.feedback'],
+                                $app['logger.system'],
+                                $app['mailer']
+                            );
+                        }
+                    ),
+                    'email'    => $app->share(
+                        function () use ($app) {
+                            return new Submission\Handler\Email(
+                                $app['boltforms.config'],
+                                $app['storage'],
+                                $app['boltforms.feedback'],
+                                $app['logger.system'],
+                                $app['mailer'],
+                                $app['dispatcher'],
+                                $app['twig'],
+                                $app['url_generator']
+                            );
+                        }
+                    ),
+                    'redirect' => $app->share(
+                        function () use ($app) {
+                            return new Submission\Handler\Redirect($app['url_matcher']);
+                        }
+                    ),
                 ]);
             }
         );
