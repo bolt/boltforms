79e19a839c2052fe3e2ee82b7f3ecffdf6175f59
Gawain Lynch <gawain.lynch@gmail.com>
2016-09-14 19:45:34 +0200

    [Tetsts] Remove old tests

--- tests/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once __DIR__ . '/../vendor/bolt/bolt/tests/phpunit/bootstrap.php';
-
-define('EXTENSION_TEST_ROOT', dirname(__DIR__));
-define('EXTENSION_AUTOLOAD',  realpath(dirname(__DIR__) . '/vendor/autoload.php'));
-
-require_once EXTENSION_AUTOLOAD;
